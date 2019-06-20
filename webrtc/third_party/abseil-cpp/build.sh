b4e32e8deb640ac1ee47c3b2573ed3fe6f824c4a
walletiger <walletiger@163.com>
2019-06-20 08:12:06 +0800

    abseil-cpp auto compiling

--- webrtc/third_party/abseil-cpp/build.sh
+++ webrtc/third_party/abseil-cpp/build.sh
@@ -1,3 +1,6 @@
+rm -fr build
 mkdir build 
 cd build 
-make  -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=$PWD/../../_install  ../
+cmake  -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=$PWD/../../_install  ../
+make -j8
+make install -j8
